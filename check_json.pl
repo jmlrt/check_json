fe4365ac8fe5dde652a6fd1b1dddf4f2336e3f9c
Julien Mailleret <jmlrt@users.noreply.github.com>
2014-10-20 17:24:01 +0200

    Fork check_json

--- check_json.pl
+++ check_json.pl
@@ -13,78 +13,85 @@ my $VERSION = "1.01";
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 
 # nagios exit codes
- use constant EXIT_OK            => 0;
- use constant EXIT_WARNING       => 1;
- use constant EXIT_CRITICAL      => 2;
- use constant EXIT_UNKNOWN       => 3;
-
-
+use constant EXIT_OK            => 0;
+use constant EXIT_WARNING       => 1;
+use constant EXIT_CRITICAL      => 2;
+use constant EXIT_UNKNOWN       => 3;
 my $status = EXIT_UNKNOWN;
 
 #parse cmd opts
 my %opts;
 getopts('vU:t:d:', \%opts);
 $opts{t} = 5 unless (defined $opts{t});
-if (not (defined $opts{U}) ) {
-        print "ERROR: INVALID USAGE\n";
-        HELP_MESSAGE();
-        exit $status;
+my $timeout = $opts{t};
+my $url = $opts{U};
+my $file = $opts{d};
+if (not (defined $url) )
+{
+  print "ERROR: INVALID USAGE\n";
+  HELP_MESSAGE();
+  exit $status;
 }
 
+#initialize useragent
 my $ua = LWP::UserAgent->new;
 
 $ua->agent('Redirect Bot ' . $VERSION);
 $ua->protocols_allowed( [ 'http', 'https'] );
 $ua->parse_head(0);
-$ua->timeout($opts{t});
+$ua->timeout($timeout);
 
-my $response = $ua->get($opts{U});
+#get http
+my $response = $ua->get($url);
 
+#exit if result is not json
 if ( index($response->header("content-type"), 'application/json') == -1 )
 {
   print "Expected content-type to be application/json, got ", $response->header("content-type");
   exit EXIT_CRITICAL;
 }
 
+eval
+{
 
-my $json_response;
-
-eval {
-
-  $json_response = decode_json($response->content);
-  print "JSON repsonse decoded successfully.";
+  my $json_response = decode_json($response->content);
+  my %json_hash = %{$json_response};
 
   $status = EXIT_OK;
+  if ($file)
+  {
 
-  if ($opts{d}) {
+    if ( -e $file)
+    {
 
-    if ( -e $opts{d}) {
-
-      my $hash_import = do $opts{d};
-      
+      open( my $fh, '<', $file );
+      my $json_text = <$fh>;
+      my $hash_import = decode_json( $json_text );
       my %attr_check = %{$hash_import};
 
       my @errors;
 
-      for my $key (sort keys %attr_check) {
-          for my $attr (sort keys %{$attr_check{$key}}) {
-              my $have = $json_response->{products}{$key}{now}{$attr};
-              my $expect = $attr_check{$key}{$attr};
-              push @errors, "For key $key, attribute $attr, expected '$expect', but got '$have'"
-                  unless $have eq $expect;
-          }
+      for my $key (sort keys %attr_check)
+      {
+        my $attr = $attr_check{$key};
+        my $have = $json_hash{$key};
+        push @errors, "$key: $have"
+          unless $have eq $attr;
       }
 
-      if (@errors) {
-          print "Errors:\n", map { "$_\n" } @errors;
-          $status = EXIT_CRITICAL;
+      if (@errors)
+      {
+        print map { "$_, " } @errors;
+        $status = EXIT_CRITICAL;
       }
-      else {
-          print "Found expected content.";
-          $status = EXIT_OK;
-      } 
+      else
+      {
+        print "Found expected content.";
+        $status = EXIT_OK;
+      }
     }
-    else {
+    else
+    {
       print "Unable to find data file $opts{d}";
       $status = EXIT_UNKNOWN;
     }
@@ -92,34 +99,35 @@ eval {
 
   exit $status;
 
-} or do {
-  print "Unable to decode JSON, invalid response?";
-  exit EXIT_CRITICAL;
+}
+or do
+{
+        print "Unable to decode JSON, invalid response?";
+        exit EXIT_CRITICAL;
 };
 
-sub HELP_MESSAGE 
+sub HELP_MESSAGE
 {
-        print <<EOHELP
-        Retrieve an http/s url and checks its application type is application/json and the response content decodes properly into JSON.  
-        Optionally verify content is found using data file.
-        
-        --help      shows this message
-        --version   shows version information
+  print <<EOHELP
+Retrieve an http/s url and checks its application type is application/json and the response content decodes properly into JSON.
+Optionally verify content is found using data file.
 
-        USAGE: $0 -U http://my.url.com [-d sample.data]
+  --help      shows this message
+  --version   shows version information
 
-        -U          URL to retrieve (http or https)
-        -d          absolute path to data file containing hash to find with JSON response (optional)
-        -t          Timeout in seconds to wait for the URL to load (default 60)
+USAGE: $0 -U http://my.url.com [-d sample.data]
+
+  -U          URL to retrieve (http or https)
+  -d          absolute path to data file containing hash to find with JSON response (optional)
+  -t          Timeout in seconds to wait for the URL to load (default 60)
 
 EOHELP
 ;
 }
 
-
-sub VERSION_MESSAGE 
+sub VERSION_MESSAGE
 {
-        print <<EOVM
+  print <<EOVM
 $plugin_name v. $VERSION
 Copyright 2012, Brian Buchalter, http://www.endpoint.com - Licensed under GPLv2
 EOVM
